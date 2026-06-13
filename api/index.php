da26a7451fe86a61e72ba9d7971698d8b669dd5b
Zeyphrin <yakariayahya50@gmail.com>
2026-06-14 00:40:03 +0700

    update api test1

--- api/index.php
+++ api/index.php
@@ -1,17 +1,3 @@
 <?php
 
-// 1. Memaksa Laravel memindahkan semua file cache internal ke folder /tmp
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-putenv('APP_EVENTS_CACHE=/tmp/events.php');
-putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
-putenv('APP_ROUTES_CACHE=/tmp/routes.php');
-putenv('APP_SERVICES_CACHE=/tmp/services.php');
-putenv('VIEW_COMPILED_PATH=/tmp');
-
-// 2. Memastikan driver yang digunakan ramah Serverless
-putenv('CACHE_DRIVER=array');
-putenv('LOG_CHANNEL=stderr');
-putenv('SESSION_DRIVER=cookie');
-
-// 3. Menjalankan Laravel seperti biasa
 require __DIR__ . '/../public/index.php';
