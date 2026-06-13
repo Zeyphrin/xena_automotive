4dbe0b3a42054f3778d77d8b82aeaa0e0bce2ed2
Zeyphrin <yakariayahya50@gmail.com>
2026-06-14 00:31:17 +0700

    update api vercel + del cache

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
-// Meneruskan request dari Vercel ke public/index.php Laravel
+
+// 1. Memaksa Laravel memindahkan semua file cache internal ke folder /tmp
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
+// 2. Memastikan driver yang digunakan ramah Serverless
+putenv('CACHE_DRIVER=array');
+putenv('LOG_CHANNEL=stderr');
+putenv('SESSION_DRIVER=cookie');
+
+// 3. Menjalankan Laravel seperti biasa
 require __DIR__ . '/../public/index.php';
