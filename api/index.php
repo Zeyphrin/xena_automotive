59b42a6d0a1f120e7645e5dbc6e27885b14f66d8
Zeyphrin <yakariayahya50@gmail.com>
2026-06-14 01:29:17 +0700

    vercel setup claude3

--- api/index.php
+++ api/index.php
@@ -1,3 +1,6 @@
 <?php
-echo "PHP is working. Path: " . __DIR__;
-phpinfo();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . '/../public/index.php';
